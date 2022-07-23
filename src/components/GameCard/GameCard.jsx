df15e25071e7b6240bc0ac720175fabe4ce047fc
nicxzmiller <nicxzmiller2@gmail.com>
2022-07-23 05:46:38 +0200

    10%

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Cover from '../../img/cover.jpg'
+import Profile from '../../img/profileImg.jpg'
+import './GameCard.css'
 
 const GameCard = () => {
   return (
     <div className='gameCard'>
-        
+        <div className='gameImages'>
+          <img src={Cover} alt="" />
+          <img src={Profile} alt="" />
+        </div>
+
+        <div className='gameName'>
+          <span>BLACKJACK</span>
+          <span>American BlackJack</span>
+        </div>
+
+        <div className='gameStatus'>
+          <hr />
+          <div>
+            <div className='bets'>
+              <span>10</span>
+              <span>Bet Types</span>
+            </div>
+            <div className='vl'></div>
+            <div className='bets'>
+              <span>12</span>
+              <span>Side Bets</span>
+            </div>
+          </div>
+          <hr />
+        </div>
     </div>
   )
 }
